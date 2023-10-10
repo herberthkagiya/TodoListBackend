f06264dc3923a46c7038aaac1195e4d9eb0c14de
Herberth Kagiya <tayki.heberth@gmail.com>
2023-10-10 20:12:12 -0300

    FEAT: User registration to database working

---
+++ src/main/java/com/kagiya/todolist/user/UserController.java
@@ -0,0 +1,37 @@
+package com.kagiya.todolist.user;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private IUserRepository repository;
+
+    @PostMapping("")
+    public ResponseEntity create(
+        @RequestBody UserModel newUser
+    ){
+
+        var user = this.repository.findByUsername(newUser.getUsername());
+        
+        if(user != null){
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("User already exists");
+        }
+
+        var userCreated = this.repository.save(newUser);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(userCreated);
+    }
+}
