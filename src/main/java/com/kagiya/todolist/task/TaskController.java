4b984c0d457e35beea150ea50af737b3f9999c6a
Herberth Kagiya <tayki.heberth@gmail.com>
2023-10-12 13:12:38 -0300

    FEAT: Validation at task dates implemented

--- src/main/java/com/kagiya/todolist/task/TaskController.java
+++ src/main/java/com/kagiya/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package com.kagiya.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +21,26 @@ public class TaskController {
     private ITaskRepostiory repostiory;
 
     @PostMapping("")
-    public TaskModel create(
+    public ResponseEntity create(
         @RequestBody TaskModel newTask, 
         HttpServletRequest request
     ){
         var idUser = request.getAttribute("idUser");
         newTask.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();  
+        if(
+            currentDate.isAfter(newTask.getStartAt()) || 
+            currentDate.isAfter(newTask.getEndAt())
+        ){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start and end date must be higher than current");
+        }
+
+        if(newTask.getStartAt().isAfter(newTask.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start must be before end date");
+        }
+
         var createdTask = this.repostiory.save(newTask);
-        return createdTask;
+        return ResponseEntity.status(HttpStatus.OK).body(createdTask);
     }
 }
